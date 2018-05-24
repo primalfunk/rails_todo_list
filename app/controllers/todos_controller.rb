134c3358695e3a06ee715ecbbe65f102845b90a8
Jared Menard <jared.d.menard@gmail.com>
2018-05-24 16:59:14 -0600

    initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,48 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+   end
+    
+   def show
+     @todo = Todo.find(params[:id])
+   end
+       
+   def new
+    @todos = Todo.new
+   end
+
+   def create
+    @todo = Todo.new(todos_params)
+ 
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
+ 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+  
+    if @todo.update(todo_params)
+      redirect_to todos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+    redirect_to todos_path
+  end
+
+  private
+ 
+    def todos_params
+      params.require(:todo).permit(:desc, :due_by)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+  resources :todos
+end
